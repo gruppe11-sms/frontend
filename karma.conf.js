90b9ce9273db83b18a8566389047e198aa3d7ea2
Zlepper <hansen13579@gmail.com>
2017-09-14 20:54:18 +0200

    Actually add Teamcity reporter to karma conf - take 2

--- karma.conf.js
+++ karma.conf.js
@@ -29,6 +29,19 @@ module.exports = function (config) {
     logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: ['Chrome'],
-    singleRun: false
+    singleRun: false,
+    customLaunchers: {
+      ChromeCanaryHeadless: {
+        base: 'ChromeHeadless',
+        flags: [
+          '--no-sandbox',
+          // See https://chromium.googlesource.com/chromium/src/+/lkgr/headless/README.md
+          '--headless',
+          '--disable-gpu',
+          // Without a remote debugging port, Google Chrome exits immediately.
+          ' --remote-debugging-port=9222',
+        ]
+      }
+    },
   });
 };
