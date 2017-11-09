a087a17db1dbc532155512f7a4a5ef7b207b76c6
MadsLB <madslbach@gmail.com>
2017-11-09 12:14:19 +0100

    SMS-126 : Started SMS-128 : Started

---
+++ src/app/course/course/lesson/lesson.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import {CourseService} from '../../course.service';
+import {Observable} from 'rxjs/Observable';
+import {Lesson} from '../../models/lesson';
+import {ActivatedRoute} from '@angular/router';
+
+@Component({
+  selector: 'app-lesson',
+  templateUrl: './lesson.component.html',
+  styleUrls: ['./lesson.component.scss']
+})
+export class LessonComponent implements OnInit {
+  lessons: Observable<Lesson[]>;
+  constructor(private courseService: CourseService, private route: ActivatedRoute) { }
+
+  ngOnInit() {
+    this.lessons = this.route.params
+      .map(params => {
+        return Number(params.id);
+      })
+      .switchMap(id => {
+        return this.courseService.getLessons(Number(id));
+      });
+  }
+
+}
