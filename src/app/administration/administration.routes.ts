06ed4c8a8c1192abda84e4a9e5d7408184ffc2d2
Jens Navne <jens.navne@gmail.com>
2017-11-09 13:01:00 +0100

    Edit user is working

--- src/app/administration/administration.routes.ts
+++ src/app/administration/administration.routes.ts
@@ -1,20 +1,25 @@
-import {RouterModule, Routes} from "@angular/router";
-import {AddUserComponent} from "./add_user/add-user.component";
-import {AdministrationComponent} from "./administration/administration.component";
-import {EditUserFilterComponent} from "./edit-user-filter/edit-user-filter.component";
+import {RouterModule, Routes} from '@angular/router';
+import {AddUserComponent} from './add_user/add-user.component';
+import {AdministrationComponent} from './administration/administration.component';
+import {EditUserFilterComponent} from './edit-user-filter/edit-user-filter.component';
+import {EditUserComponent} from './edit-user-filter/edit-user/edit-user.component';
 
 export const administrationRoutes: Routes = [
   {
-    path: '',
-    component: AdministrationComponent
+    path: 'users/edit/:id',
+    component: EditUserComponent,
   },
   {
     path: 'users/new',
-    component: AddUserComponent
+    component: AddUserComponent,
   },
   {
     path: 'users/edit',
-    component: EditUserFilterComponent
+    component: EditUserFilterComponent,
+  },
+  {
+    path: '',
+    component: AdministrationComponent,
   },
 ];
 
--- src/app/administration/edit-user-filter/edit-user-filter.component.ts
+++ src/app/administration/edit-user-filter/edit-user-filter.component.ts
@@ -1,59 +1,26 @@
 import {Component, OnInit} from '@angular/core';
-import {FormControl} from "@angular/forms";
-import {Observable} from "rxjs/Observable";
-import {UserService} from "../../services/user.service";
-import {User} from "../../models/user";
-import "rxjs/add/operator/startWith";
+import {Observable} from 'rxjs/Observable';
+import {UserService} from '../../services/user.service';
+import {User} from '../../models/user';
+import 'rxjs/add/operator/startWith';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/zip';
-import {Role} from "../../models/role";
-import {RoleService} from "../../services/role.service";
-import {Group} from "../../models/group";
-import {GroupService} from "../../services/group.service";
 
 @Component({
   selector: 'app-edit-user-filter',
   templateUrl: './edit-user-filter.component.html',
-  styleUrls: ['./edit-user-filter.component.scss']
+  styleUrls: ['./edit-user-filter.component.scss'],
 })
 export class EditUserFilterComponent implements OnInit {
 
-  public myControl = new FormControl();
-  public users: User[] = [];
-  public filteredOptions: Observable<User[]>;
-  public user: Observable<User>;
+  public users: Observable<User[]>;
 
-  public roles: Observable<Role[]>;
-  public groups: Observable<Group[]>;
-
-  constructor(private userService: UserService,
-              private roleService: RoleService,
-              private groupService: GroupService) {
+  constructor(private userService: UserService) {
   }
 
   ngOnInit() {
-    this.roles = this.roleService.getRoles();
-    this.groups = this.groupService.getGroups();
-    const users = this.userService.getUsers(); //.subscribe(users => this.users = users, error => console.log('Could not connect to role server'));
-
-    this.filteredOptions = users.zip(
-      this.myControl.valueChanges
-        .startWith(null)
-        .map(user => user && typeof user === 'object' ? user.name : user)
-    ).map(([users, name]) => name ? this.filter(name) : users.slice());
-
-    this.user = this.myControl.valueChanges.startWith(null);
+    this.users = this.userService.getUsers();
   }
-
-  public filter(name: string): User[] {
-    return this.users.filter(option =>
-      option.name.toLowerCase().indexOf(name.toLowerCase()) === 0)
-  }
-
-  public displayFn(user: User): string {
-    return user ? user.name : user;
-  }
-
 }
 
 
